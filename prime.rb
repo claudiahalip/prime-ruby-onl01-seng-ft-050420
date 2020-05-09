21e7683e4b33e9651d205836ad5e2e8442e2f266
claudia v <claudia4ro@yahoo.com>
2020-05-09 14:55:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,9 +6,9 @@ def prime?(num)
   if num <= 0 
     return false 
   else
-    (2..num-1).each do |n|
+    (2...num).each do |n|
       if  num % n == 0
-        return false
+        false
       end
     true
     end
