9d844bdad5b565a65a947df3b1f2292bda40b14e
claudia v <claudia4ro@yahoo.com>
2020-05-09 15:07:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+require 'pry'
+
+def prime?(num)
+  if num <= 1 
+    return false 
+  else
+    (2...num).each do |n|
+      if  num % n == 0
+        return false
+      end
+    end
+  true
+  end
+end
