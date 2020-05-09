efb8825d9025364a1aaed3af2d310eab86975ecc
claudia v <claudia4ro@yahoo.com>
2020-05-09 14:37:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+require 'pry'
+
+def prime?(num)
+  if num <= 0 
+    return false 
+  elsif
+    (2..num/2).each do |n|
+      if  num % n == 0
+        true
+      end
+    end
+  else
+   false
+    
+  end
+  
+ 
+end
